7507b9a0ad61ca16ac01852e489e1d220b1af2be
Seraph <seraph.axl@gmail.com>
2025-04-07 01:57:19 -0600

    Actualizar la configuración de Tailwind CSS para ajustar colores y simplificar la sintaxis

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,57 @@
-import { nextui } from '@nextui-org/react';
+import { nextui } from '@nextui-org/react'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: {
+          DEFAULT: '#000000',
           50: '#f0fdf4',
           100: '#dcfce7',
           200: '#bbf7d0',
           300: '#86efac',
           400: '#4ade80',
-          500: '#22c55e',
+          500: '#000000',
           600: '#16a34a',
           700: '#15803d',
           800: '#166534',
           900: '#14532d',
           950: '#052e16',
-        },
+          default: '#000'
+        }
       },
       animation: {
         'slide-in': 'slide-in 0.3s ease-out',
-        'slide-out': 'slide-out 0.3s ease-in',
+        'slide-out': 'slide-out 0.3s ease-in'
       },
       keyframes: {
         'slide-in': {
           '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(0)' }
         },
         'slide-out': {
           '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-      },
-    },
+          '100%': { transform: 'translateX(100%)' }
+        }
+      }
+    }
   },
-  darkMode: "class",
-  plugins: [nextui({
-    themes: {
-      light: {
-        colors: {
-          primary: {
-            DEFAULT: "#16a34a",
-            foreground: "#ffffff",
-          },
-          focus: "#16a34a",
-        },
-      },
-    },
-  })],
-};
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: '#16a34a',
+              foreground: '#ffffff'
+            },
+            focus: '#16a34a'
+          }
+        }
+      }
+    })
+  ]
+}
